a553134243fa59631f0c81df81b512723179aac7
VF <vasul.fr@gmail.com>
2023-03-02 23:03:43 +0200

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { UserComponent } from './components/user/user.component';
-import {HttpClientModule} from "@angular/common/http";
+import { PostsComponent } from './components/posts/posts.component';
+import { MainLayoutsComponent } from './latouts/main-layouts/main-layouts.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutsComponent, children: [
+      {path:'', redirectTo: 'users', pathMatch: 'full'},
+      {path:'users', component: UsersComponent, children: [
+          {path:':id', component: UserDetailsComponent},
+        ]},
+      {path:'posts', component: PostsComponent},
+    ]
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    PostsComponent,
+    MainLayoutsComponent,
+    HeaderComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {IUser} from "../../interfaces";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-user',
@@ -9,9 +10,24 @@ import {IUser} from "../../interfaces";
 export class UserComponent implements OnInit {
   @Input()
   user: IUser;
-  constructor() {
+  //передаю дані вверх по компонентах
+  @Output()
+  liftUser = new EventEmitter<IUser>()
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+  }
+  //передаю дані вверх по компонентах
+  list(): void {
+    this.liftUser.emit(this.user)
+  }
+  // send data in state button
+  getDetails() {
+    this.router.navigate([this.user.id], {
+      relativeTo: this.activatedRoute,
+      state:{user: this.user}
+    })
   }
 }
